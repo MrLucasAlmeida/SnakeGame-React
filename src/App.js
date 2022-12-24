7d76a027bf668d301476ca1c6918df3d939de814
MrLucasAlmeida <lucasoa2701@live.com>
2022-12-24 15:46:08 -0800

    updated game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import Board from './Board/Board.jsx';
 import './App.css';
-import Board from './Components/Board';
-function App() {
-  return (
-    <div>
-      <Board />
-    </div>
-  );
-}
+
+const App = () => (
+  <div className="App">
+    <Board></Board>
+  </div>
+);
 
 export default App;
